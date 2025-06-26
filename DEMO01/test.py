eb7d3b969e7c06dfecd76a92a3f24007ba5127b9
EDX5555 <tosetancos5@gmail.com>
2025-06-26 17:33:50 -0500

    modificando 2 archivos

--- DEMO01/test.py
+++ DEMO01/test.py
@@ -1,10 +1,3 @@
 # This program adds two numbers and displays the sum
 
-num1 = 10
-num2 = 25
-
-# Calculate the sum
-sum_of_numbers = num1 + num2
-
-# Print the result
-print(f"The sum of {num1} and {num2} is: {sum_of_numbers}")
+print("hello world")
--- demo.py
+++ demo.py
@@ -1,10 +1,5 @@
 # This program adds two numbers and displays the sum
 
-num1 = 10
-num2 = -5
+for i in range(5):
+    print("hi",i)
 
-# Calculate the sum
-sum_of_numbers = num1 + num2
-
-# Print the result
-print(f"The sum of {num1} and {num2} is: {sum_of_numbers}")
